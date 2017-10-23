90e6aa52970010d7f15af14991dd487bb06a42db
Sam Holdcroft <sam.holdcroft42@gmail.com>
2017-10-23 00:02:51 -0700

    Added the start of the Natural Language Interface

--- project.pl
+++ project.pl
@@ -1,5 +1,54 @@
 %%  Cluedo Project  %%
 
+%%Natural Language Interface
+
+%noun_phrase(T0, T2, Ind, C0, C2) is true if:
+%   the difference list between T0 and T2 is a noun phrase,
+%   Ind is the individual reffered to by the noun phrase.
+%   The difference list between C0 and C2 are the constrains imposed by the noun phrase.
+%In the language of Cluedo, a noun phrase is a determiner followed by a noun, followed by modifying phrase.
+noun_phrase(T0, T3, Ind, C0, C3) :-
+    det(T0, T1, Ind, C0, C1),
+    noun(T1, T2, Ind, C1, C2),
+    mp(T2, T3, Ind, C2, C3).
+
+%Determiners can be ignored.
+det([the | T],T,_,C,C).
+det(T,T,_,C,C).
+
+%A modofying phrase is either nothing or a relation between two objects followed by a noun phrase that describes the second object.
+mp(T,T,_,C,C).
+%mp(T0,T2, O1, C0, C2) :-
+%    reln(T0,T1,O1,O2,C0,C1),
+%    noun_phrase(T1,T2,O2,C1,C2).
+
+
+%A question can have multiple starts, which can be ignored.
+question([who,has | T0],T1,Ind,C0,C1) :-
+    noun_phrase(T0,T1,Ind,C0,C1).
+
+%ask(Q, A) is true if A is the answer to the question A.
+%Q is given as a string, which is then converted to a list of lower case atoms.
+ask(Q,A) :-
+    string_lower(Q, QLowerCase),
+    tokenize_atom(QLowerCase, QList),
+    question(QList,[],A,C,[]),
+    prove_all(C).
+%A question may also end in a question mark.
+ask(Q,A) :-
+    string_lower(Q, QLowerCase),
+    tokenize_atom(QLowerCase, QList),
+    question(QList,[?],A,C,[]),
+    prove_all(C).
+
+% prove_all(L) proves all elements of L against the database
+prove_all([]).
+prove_all([H|T]) :-
+    call(H),      % built-in Prolog predicate calls an atom
+    prove_all(T).
+
+%%Notebook Functionality
+
 %nextQuestion(T,Q) is true if T is the type of the answer R, where R is the next question the program reccomends the player asks.
 %   -If there are no possble answers, the type is 'error'
 %   -If there is exactly one possible answer, the type is 'answer'
@@ -48,26 +97,35 @@ suspects([H|T], R) :-  prop(_, has, H),
                        suspects(T, R).
                           
 %add(P, C) returns true if card C exists, and if the prop(P, has, C) is added to the Knowledge Base.
-add(P, C) :- characters(L),
-             contains(C, L),
-             assertz(prop(P, has, C)).
-add(P, C) :- weapons(L),
-             contains(C, L),
-             assertz(prop(P, has, C)).
-add(P, C) :- rooms(L),
-             contains(C, L),
+add(P, C) :- isCard(C),
+             isPlayer(P),
              assertz(prop(P, has, C)).
 
+%exists(C) returns true if the card C exists.
+isCard(C) :- characters(L),
+             contains(C, L).
+isCard(C) :- weapons(L),
+             contains(C, L).
+isCard(C) :- rooms(L),
+             contains(C, L).
+
+%isPlayer(P) returns true if P is a player.
+isPlayer(P) :- players(L),
+               contains(P, L).
 
 %contains(E, L) returns true if E is an element of L.
 contains(E, [E|_]).
 contains(E, [_|T]) :- contains(E, T).
+
 %% Knowledge Base %%
 
 % The player represented by the program.
 me(p1).
 % The current room.
 current_room(kitchen).
+
+% List of all the players.
+players([p1, p2, p3, p4]).
 
 % List of all the characters
 characters([mrs_scarlett, colonel_mustard, mrs_white, reverend_green, mrs_peacock, professor_plum]).
@@ -83,3 +141,19 @@ prop(p1, has, lead_pipe).
 prop(p1, has, dagger).
 prop(p1, has, ballroom).
 prop(p4, has, colonel_mustard).
+
+%%Dictionary
+
+%A noun is the name of a card, if the card exists.
+noun([Card | T],T,Ind,[prop(Ind,has,Card)|C],C) :- isCard(Card).
+
+%Some cards are formed of two words seperated by a '_'. e.g. mrs_scarlett.
+noun([Card1, Card2 | T],T,Ind,[prop(Ind,has,Card)|C],C) :-
+    atom_concat(Card1, '_', R1),
+    atom_concat(R1, Card2, Card),
+    isCard(Card).
+
+%A noun is a players name.
+noun([Name | T],T,Name,C,C) :- isPlayer(Name).
+    
+
